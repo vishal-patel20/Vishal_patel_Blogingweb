54c8fb28085880264189e7d43665b15eff12692e
vishal-patel20 <shivam1705x@gmail.com>
2026-02-02 21:47:56 +0530

    Fix deployment errors: update config, scripts, and dependencies

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express=require("express")
+const express = require("express")
 const path = require("path")
-const userroute=require("./routes/user")
-const userBlog=require("./routes/blog")
+const userroute = require("./routes/user")
+const userBlog = require("./routes/blog")
 const Blog = require("./models/blog")
 
 const mongoose = require("mongoose");
-const cookieparser=require("cookie-parser");
+const cookieparser = require("cookie-parser");
 const { checkforAuthenticationcookie } = require("./Middleware/authentication");
 
-const app =express()
-const port=8000;
+const app = express()
+const port = process.env.PORT || 8000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/Blogify").then((e)=>console.log("mongodb connected"))
+mongoose.connect(process.env.Mongo_url || "mongodb://127.0.0.1:27017/Blogify").then((e) => console.log("mongodb connected"))
 
-app.set("view engine","ejs")
-app.set("views",path.resolve("views/"))
+app.set("view engine", "ejs")
+app.set("views", path.resolve("views/"))
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieparser())
 app.use(checkforAuthenticationcookie("Token"))
 app.use(express.static(path.resolve("./public")))
 
-app.get("/",async (req,res)=>{
-    const allBlogs= await Blog.find({})
-    res.render("Home",{
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({})
+    res.render("Home", {
         user: req.user,
-        blogs:allBlogs
+        blogs: allBlogs
     })
 })
-app.use("/user",userroute)
-app.use("/Blog",userBlog)
+app.use("/user", userroute)
+app.use("/Blog", userBlog)
 
-app.listen(port,console.log(`the server start at the ${port}`))
+app.listen(port, () => console.log(`the server start at the ${port}`))
