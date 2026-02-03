4ffce938be4466699f947186dfae92a3e126d32f
vishal-patel20 <shivam1705x@gmail.com>
2026-02-03 06:34:23 +0530

    vishal commit

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,47 @@
-const{ Router }=require("express")
-const route=Router()
+const { Router } = require("express")
+const route = Router()
 const user = require("../models/user")
 
 
-route.get("/signin",(req,res)=>{
+route.get("/signin", (req, res) => {
     return res.render("signin")
 })
-route.get("/signup",(req,res)=>{
+route.get("/signup", (req, res) => {
     return res.render("signup")
 })
-route.post("/signup",async(req,res)=>{
-    const {fullname,email,password} = req.body
-    await user.create({
-        fullname,
-        email,
-        password
-    })
-    return res.redirect("/")
-})
-route.post("/signin",async(req,res)=>{
-    const {email,password} = req.body
+route.post("/signup", async (req, res) => {
+    const { fullname, email, password } = req.body
     try {
-         const Token = await user.matchpasswordandtokengen(email,password)
-//  console.log("Token",Token);
-    return res.cookie("Token",Token).redirect("/")
+        await user.create({
+            fullname,
+            email,
+            password
+        })
+        return res.redirect("/user/signin")
     } catch (error) {
-        return res.render("signin",{error:"incorect email and password"})   
+        if (error.code === 11000) {
+            return res.render("signup", {
+                error: "User already exists with this email address."
+            })
+        }
+        return res.render("signup", {
+            error: "Something went wrong. Please try again."
+        })
+    }
+})
+route.post("/signin", async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const Token = await user.matchpasswordandtokengen(email, password)
+        //  console.log("Token",Token);
+        return res.cookie("Token", Token).redirect("/")
+    } catch (error) {
+        return res.render("signin", { error: "incorect email and password" })
     }
 
 })
-route.get("/logout", (req,res)=>
-{
+route.get("/logout", (req, res) => {
     res.clearCookie("Token").redirect("/")
 })
 
-module.exports=route 
+module.exports = route 
